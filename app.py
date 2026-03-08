75202d7f3f69083a2616dd9261a861a8b02320ee
MANU KRISHNA P V <manuvellanchery@gmail.com>
2026-03-08 19:12:27 +0530

    Fix render port binding

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 
 
@@ -101,5 +102,7 @@ def login():
 
     return render_template("login.html", message=message)
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
